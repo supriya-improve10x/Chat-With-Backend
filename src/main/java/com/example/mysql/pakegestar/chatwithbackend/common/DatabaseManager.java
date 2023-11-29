90a7f09c54972cb8cf70d7cbf968d8899157679f
Supriya <supriya.singamsetty@improve10x.com>
2023-11-29 15:35:21 +0530

    code cleanup

---
+++ src/main/java/com/example/mysql/pakegestar/chatwithbackend/common/DatabaseManager.java
@@ -0,0 +1,23 @@
+package com.example.mysql.pakegestar.chatwithbackend.common;
+
+import java.sql.*;
+
+public abstract class DatabaseManager {
+    protected Connection createConnection() throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        String url = "jdbc:mysql://db4free.net:3306/improve10x";
+        String username = "trainee10x";
+        String password = "12345678";
+        return DriverManager.getConnection(url, username, password);
+    }
+
+    protected <T> T runQuery(String query) throws SQLException, ClassNotFoundException {
+        Connection connection = createConnection();
+        Statement statement = connection.createStatement();
+        ResultSet resultSet = statement.executeQuery(query);
+        T result = convertToObject(resultSet);
+        connection.close();
+        return result;
+    }
+    protected abstract <T> T convertToObject(ResultSet resultSet) throws SQLException;
+}
